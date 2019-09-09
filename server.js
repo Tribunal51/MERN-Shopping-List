5ceae36fa7581d3d6eb3df70154dafabcb3fb2d4
Zohair Irani <zohairirani@gmail.com>
2019-09-09 13:57:43 +0300

    Version 1.0.1

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 
+
+
+// Connect to Mongo
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(error => console.log(error));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
 // Use Routes
 app.use('/api/items', items);
 
@@ -29,13 +40,4 @@ if(process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
-
-// Connect to Mongo
-mongoose.connect(db)
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(error => console.log(error));
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
 
